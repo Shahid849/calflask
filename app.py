0608f33da970f2f13bfe3443fec14f7cfe9bacb1
shahid849 <khanshahid1564@gmail.com>
2023-09-16 09:51:47 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,request,render_template
+app=Flask(__name__)
+@app.route('/')
+def welcome():
+    return "<h2>welcome to the Flask</h2>"
+@app.route('/cal',method=["GET"])
+def math_operator():
+    operation=request.json["operation"]
+    number1=request.json["number1"]
+    
+    number2=request.json["number2"]
+    if operation=="add"
+        result=number1+number2
+    elif operation=="multiply":
+        result=number1*number2
+    elif operation=="division":
+        result=number1/number2
+    else:
+        result=number1-number2
+    return result
+if __name__=='__main__':
+    app.run()
